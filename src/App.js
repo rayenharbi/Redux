2b374621aaed76988b440d444ff48a6d86dc76e4
rayenharbi <rayenharbi123@gmail.com>
2023-05-06 14:53:35 +0100

    redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React,{useState} from 'react'
+import {useSelector,useDispatch} from "react-redux"
+import {addTodo,deleteTodo,completeTodo,updateTodo} from './actions/todoAction'
+import {Modal,Button} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const [task,setTask]=useState("")
+  const [editTask,setEditTask]=useState("")
+  const [filter,setFilter]=useState("all")
+  const todos=useSelector(state=>state.todoReducer)
+  const dispatch=useDispatch()
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <input type="text" placeholder='add task...' onChange={(e)=>setTask(e.target.value)}/>
+        <button onClick={()=>dispatch(addTodo(task))}>add task</button>
+        <button onClick={()=>setFilter("all")}>all</button>
+        <button onClick={()=>setFilter("done")}>done</button>
+        <button onClick={()=>setFilter("undone")}>undone</button>
+      { filter==="all" ? todos.map(el=><div>
+        <h2>{el.title}</h2>
+        
+        <Button variant="primary" onClick={handleShow}>
+        update
+      </Button>
+      <Modal show={show} onHide={handleClose}>
+        
+        <Modal.Body>
+          <input type="text" placeholder='edit task...' value={editTask} onChange={(e)=>setEditTask(e.target.value)}/>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={()=>{dispatch(updateTodo(editTask,el.id));handleClose()}}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <button onClick={()=>dispatch(deleteTodo(el.id))}>delete</button>
+        <button onClick={()=>dispatch(completeTodo(el.id))}>{el.complete ? "done":"undone"}</button>
+      </div>):filter==="done" ? todos.filter(el=>el.complete===true)
+      .map(el=><div>
+        <h2>{el.title}</h2>
+        <button onClick={()=>dispatch(deleteTodo(el.id))}>delete</button>
+        <button onClick={()=>dispatch(completeTodo(el.id))}>{el.complete ? "done":"undone"}</button>
+        </div>): todos.filter(el=>el.complete===false)
+        .map(el=><div>
+         <h2>{el.title}</h2>
+        <button onClick={()=>dispatch(deleteTodo(el.id))}>delete</button>
+        <button onClick={()=>dispatch(completeTodo(el.id))}>{el.complete ? "done":"undone"}</button>
+        </div>)
+        }
+      
     </div>
   );
 }
